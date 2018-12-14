84105b22e0c621a8bef40ecbc16f3686ab74eb37
Meenal Walokar <meenalw@mkclindia.local>
2018-12-14 16:33:35 +0530

    test

---
+++ 008_example8/numericvalue.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+import "runtime"
+
+func main(){
+	z := 76
+	y := 34.877
+	fmt.Println(z)
+	fmt.Println(y)
+	fmt.Printf("%T\n",z)
+	fmt.Printf("%T\n",y)
+
+	fmt.Printf(runtime.GOLANGOS)
+	fmt.Printf(suntime.GOARCHET)
+}
+
+//76
+//34.877
+//int
+//float64
