591e152abb128675018227cb5bc5c2f1599f874c
Meenal Walokar <meenalw@mkclindia.local>
2019-03-06 10:20:04 +0530

    new learning added

--- 057_Exercise57/channelselect.go
+++ 057_Exercise57/channelselect.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	eve := make(chan int)
 	odd := make(chan int)
@@ -13,9 +15,27 @@ func main() {
 }
 
 func send(e, o, q chan<- int) {
-
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			e <- i
+		} else {
+			o <- i
+		}
+	}
+	q <- 0
 }
 
 func receive(e, o, q <-chan int) {
-
+	for {
+		select {
+		case v := <-e:
+			fmt.Println("from eve channel:", v)
+		case v := <-o:
+			fmt.Println("from odd channel:", v)
+		case v := <-q:
+			fmt.Println("from quit channel:", v)
+			return
+		}
+		return
+	}
 }
